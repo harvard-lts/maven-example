87cf957c5e55fba06e134a7b803de9735eb4bc31
Jessi Jassal <jessica_jassal@harvard.edu>
2021-12-13 19:45:57 -0500

    Add simple test

---
+++ src/test/java/helloworld/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class HelloWorldTest {
+    
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }   
+} 
